f3a8b14e7a769c74a600e241b746ab730e802c59
tLigsP <16521178@gm.uit.edu.vn>
2019-01-07 03:17:48 +0700

    Basic menu layout

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -4,10 +4,12 @@ import { createDrawerNavigator, createAppContainer } from 'react-navigation';
 import SideBar from './SideBar/SideBar';
 import HomeScreen from './Screens/HomeScreen/';
 import FaqScreen from './Screens/FaqScreen/FaqScreen';
+import FoodMenuScreen from './Screens/FoodMenuScreen/';
 
 const MyDrawerNavigator = createDrawerNavigator({
   Home:  HomeScreen,
   FAQ:  FaqScreen,
+  FoodMenu: FoodMenuScreen,
 }, {
   contentComponent: (props) => <SideBar {...props} />,
   initialRouteName: "Home"
---
+++ src/Screens/FoodMenuScreen/data.js
@@ -0,0 +1,46 @@
+//for testing will use API for data source later
+
+export const movies = [
+  {
+    title: 'Ramen Burger',
+    poster: 'https://c8.alamy.com/comp/HGB7R0/new-fast-food-ramen-burger-close-up-on-a-paper-on-the-wooden-table-HGB7R0.jpg',
+    price: '50.000 VNĐ',
+    weight: '420g',
+  },
+  {
+    title: 'Rustic Sandwich',
+    poster: 'https://c8.alamy.com/comp/HFHJHT/rustic-sandwich-with-beefsteak-fried-egg-and-french-fries-vertical-HFHJHT.jpg',
+    price: '40.000 VNĐ',
+    weight: '420g',
+  },
+  {
+    title: 'Danish Hotdog',
+    poster: 'https://c8.alamy.com/comp/HRFARD/danish-hot-dogs-close-up-on-the-table-vertical-HRFARD.jpg',
+    price: '35.000 VNĐ',
+    weight: '420g',
+  },
+  {
+    title: 'Sloppy Joe',
+    poster: 'https://c8.alamy.com/comp/M03G1F/fast-food-sandwich-sloppy-joe-with-tomato-sauce-and-fries-close-up-M03G1F.jpg',
+    price: '50.000 VNĐ',
+    weight: '420g',
+  },
+  {
+    title: 'Bánh mì',
+    poster: 'https://www.bakersroyale.com/wp-content/uploads/2018/04/Banh-Mi-_-Bakers-Royale-630x945.jpg',
+    price: '20.000 VNĐ',
+    weight: '420g',
+  },
+  {
+    title: 'Corn dog',
+    poster: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsbBa8NJs__DbUXjqeCN8Cg28e_7ecuBtUg3m9w-JnjHtU_2lx',
+    price: '25.000 VNĐ',
+    weight: '420g',
+  },
+  {
+    title: 'Fries',
+    poster: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4Nhqd9cg_4WZn9CveB5cqL7q1jBx4MXEcj8MIkJSrVC_DvGZs',
+    price: '15.000 VNĐ',
+    weight: '420g',
+  },
+];
